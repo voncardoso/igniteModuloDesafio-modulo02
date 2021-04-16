ea5c8694f47f9e6a489b20e49494a93fd4785d6a
voncardoso <vonharrison26@gmail.com>
2021-04-16 11:43:36 -0300

    função cartItem aumount finalizado

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -26,7 +26,10 @@ const Home = (): JSX.Element => {
   const { addProduct, cart } = useCart();
 
    const cartItemsAmount = cart.reduce((sumAmount, product) => {
-      TODO
+      const newSumAmount = {...sumAmount};
+      newSumAmount[product.id] = product.amount;
+
+      return newSumAmount;
    }, {} as CartItemsAmount)
 
   useEffect(() => {
