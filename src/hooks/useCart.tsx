d4ebd10ba314b47ea0120ef75cc292e898d8ffbc
voncardoso <vonharrison26@gmail.com>
2021-04-16 11:33:33 -0300

    AddProduct finalizado

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -35,17 +35,43 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-       
-    } catch {
+      const updatedCart = [...cart]; // manter a imutabilidade, updatedCart e o novo array de cart
+      const productExists = updatedCart.find(product => product.id === productId); // verificar se a id iguais.
+      
+      const stock = await api.get(`/stock/${productId}`); // recer o id de todos os products.
+      
+      const stockAmount = stock.data.amount; // recebe a quantidade de cada product
+      const currentAmount = productExists ? productExists.amount : 0; // se produto ezxistir ele pega a quantidade se nao é 0
+      const amount = currentAmount + 1; // acrescenta mais 1 na quantidade que eu quero.
+
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if(productExists){
+        productExists.amount = amount; // productExists.amount recebe a quantidade de produto que eu quero
+      }else {
+        const product = await api.get(`/products/${productId}`); // buscar na api
+
+        const newProduct = {
+          ...product.data, // retorna os dados ja existentens
+          amount: 1, // acrescenta o amount
+        }
+        updatedCart.push(newProduct);
+      }
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart)); 
+      } catch {
       toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-       TODO
+      // TODO
     } catch {
-       TODO
+      // TODO
     }
   };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,23 +22,23 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+   const cartItemsAmount = cart.reduce((sumAmount, product) => {
+      TODO
+   }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+       TODO
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+     TODO
   }
 
   return (
@@ -50,11 +50,11 @@ const Home = (): JSX.Element => {
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+          onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
+            {cartItemsAmount[product.id] || 0} 2
           </div>
 
           <span>ADICIONAR AO CARRINHO</span>
